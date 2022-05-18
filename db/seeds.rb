73e1e02d680a076e853ca6bf4d118c646d71ea34
Moritz Metelmann <moritz@metelmann.biz>
2022-05-18 15:52:22 +0200

    Not finished but FRONTEND SUCKS

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+50.times do
+  Flat.create!(name: Faker::Games::Minecraft.achievement, address: Faker::Address.full_address, description: Faker::Hipster.paragraph, price_per_night: rand(15..10000), number_of_guests: rand(1..15))
+end
